a5dab3adcccda08ece18442e81494fbdd7120817
zhangbz <zhangbz.happyhacking@gmail.com>
2015-01-12 23:30:58 +0800

    ServiceBestPractice

---
+++ ServiceBestPractice/src/com/example/servicebestpractice/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.servicebestpractice;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent i = new Intent(this, LongRunningService.class);
+        startService(i);
+    }
+
+}
